5db91d6e26de171ed32d65ce6bcd1bab7bd86d86
Jande Royalty <janderoyalty@gmail.com>
2022-08-04 12:53:53 -0400

    It's working here. Before I try to deploy

--- wemes/settings.py
+++ wemes/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "wemes",
     "rest_framework",
     'qrcode',
+    'corsheaders',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +131,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS_ALLOWED_ORIGINS = [
+# "http://127.0.0.1:8000",
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
